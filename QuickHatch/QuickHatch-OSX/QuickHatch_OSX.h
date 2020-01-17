2b5032dd8c9470b911f2bdd5aed17ff2f7354402
Daniel Koster <danielkoster95@gmail.com>
2020-01-17 13:57:43 -0500

    refactored proj structure

---
+++ QuickHatch/QuickHatch-OSX/QuickHatch_OSX.h
@@ -0,0 +1,19 @@
+//
+//  QuickHatch_OSX.h
+//  QuickHatch-OSX
+//
+//  Created by Daniel Koster on 1/17/20.
+//  Copyright © 2020 DaVinci Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuickHatch_OSX.
+FOUNDATION_EXPORT double QuickHatch_OSXVersionNumber;
+
+//! Project version string for QuickHatch_OSX.
+FOUNDATION_EXPORT const unsigned char QuickHatch_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickHatch_OSX/PublicHeader.h>
+
+
---
+++ QuickHatch/QuickHatch-WatchOS/QuickHatch_WatchOS.h
@@ -0,0 +1,19 @@
+//
+//  QuickHatch_WatchOS.h
+//  QuickHatch-WatchOS
+//
+//  Created by Daniel Koster on 1/17/20.
+//  Copyright © 2020 DaVinci Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuickHatch_WatchOS.
+FOUNDATION_EXPORT double QuickHatch_WatchOSVersionNumber;
+
+//! Project version string for QuickHatch_WatchOS.
+FOUNDATION_EXPORT const unsigned char QuickHatch_WatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickHatch_WatchOS/PublicHeader.h>
+
+
